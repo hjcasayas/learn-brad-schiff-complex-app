6fc5f225371260526085fa7d2cb13cf72e39fb27
hjcasayas <henlyjade.casayas@gmail.com>
2024-12-19 03:35:10 +0800

    compare hashed password during login

--- web/express-ts-css/src/handlers/login-handler.ts
+++ web/express-ts-css/src/handlers/login-handler.ts
@@ -1,9 +1,23 @@
 import type { IUserService } from "@complex-app/lib-services";
+import { compare } from "bcrypt";
 import type { Handler } from "express";
 
 export const loginHandler = ({ userService }: { userService: IUserService }): Handler => {
     return async (req, res) => {
-            await userService.login(req.body);
-            res.status(200).json({ message: 'Successfully logged in!' });
+        const user = await userService.login(req.body);
+
+        if (user == null) {
+            res.status(404).json({ success: false, message: 'Either the username or password is incorrect!' });
+            return;
+        }
+
+        const isPasswordCorrect = await compare(user.password, req.body.password);
+
+        if (!isPasswordCorrect) {
+            res.status(404).json({ success: false, message: 'Either the username or password is incorrect!' });
+            return;
+        }
+
+        res.status(200).json({ success: true, message: 'Successfully logged in!' });
     }
 }
