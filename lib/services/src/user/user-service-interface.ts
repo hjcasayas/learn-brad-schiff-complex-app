598bf87c0008cc6a6c484408c2a185b0f21f3ce3
hjcasayas <henlyjade.casayas@gmail.com>
2024-12-30 10:13:58 +0800

    updated user interface

--- lib/services/src/user/user-service-interface.ts
+++ lib/services/src/user/user-service-interface.ts
@@ -1,6 +1,6 @@
 import type { UserModel } from "./index.js";
 
 export interface IUserService {
-    register: (user: UserModel) => Promise<void>;
+    register: (user: UserModel) => Promise<Omit<UserModel, 'password'> | null>;
     login: ({ username, password }: { username: string; password: string; }) => Promise<UserModel | null>;
 }
--- lib/services/src/user/user-service.ts
+++ lib/services/src/user/user-service.ts
@@ -4,15 +4,22 @@ import type { IUserService, UserModel } from "@complex-app/lib-services";
 export class UserService implements IUserService {
     constructor(private userRepository: IUserRepository) { }
 
-    register = async (userModel: UserModel): Promise<void> => {
+    register = async (userModel: UserModel): Promise<Omit<UserModel, 'password'> | null> => {
         const user: UserEntity = { ...userModel, createdDate: new Date(), updatedDate: new Date() };
-        await this.userRepository.add(user);
+        const createdUser = await this.userRepository.add(user);
+
+        if (createdUser == null) {
+            return null;
+        }
+
+        return { username: createdUser.username, email: createdUser.email };
     }
 
     login = async ({ username }: { username: string; password: string; }): Promise<UserModel | null> => {
         const user = await this.userRepository.getByUsername({ username });
+
         if (user == null) {
-            return user;
+            return null;
         }
 
         return { username: user.username, email: user.email, password: user.password };
